7c02c354f11e12b69133b02376ae64d14d516e04
Dex <lang.destini@yahoo.com>
2026-08-18 17:03:46 -0500

    Updates

--- assets/index-B4w3Ki-C.js
+++ assets/index-B4w3Ki-C.js
@@ -12431,8 +12431,9 @@ function Home() {
 					}), /* @__PURE__ */ (0, import_jsx_runtime.jsxs)("div", {
 						className: "about-right",
 						children: [
-							/* @__PURE__ */ (0, import_jsx_runtime.jsx)("h4", { children: "EXPERIENCE" }),
-							/* @__PURE__ */ (0, import_jsx_runtime.jsx)("p", { children: "Hi! I am Destini lang, a Chicago based Researcher/designer with an educational background and experience in Humanities and User-centered design.I express a strong interest in Accessibility, Design ethics, Education and Social systems. I am passionate about creating inclusive and accessible digital experiences that cater to diverse user needs. My work focuses on understanding user behavior, conducting usability research, and implementing design solutions that enhance user satisfaction and engagement." }),
+							/* @__PURE__ */ (0, import_jsx_runtime.jsx)("h4", { children: /* @__PURE__ */ (0, import_jsx_runtime.jsx)("b", { children: "EXPERIENCE" }) }),
+							/* @__PURE__ */ (0, import_jsx_runtime.jsx)("h4", { children: /* @__PURE__ */ (0, import_jsx_runtime.jsx)("b", { children: "Hi I am Destini lang " }) }),
+							/* @__PURE__ */ (0, import_jsx_runtime.jsx)("p", { children: ", a Chicago based Researcher/designer with an educational background and experience in Humanities and User-centered design.I express a strong interest in Accessibility, Design ethics, Education and Social systems. I am passionate about creating inclusive and accessible digital experiences that cater to diverse user needs. My work focuses on understanding user behavior, conducting usability research, and implementing design solutions that enhance user satisfaction and engagement." }),
 							/* @__PURE__ */ (0, import_jsx_runtime.jsxs)("div", {
 								className: "about-content",
 								children: [/* @__PURE__ */ (0, import_jsx_runtime.jsxs)("div", {
